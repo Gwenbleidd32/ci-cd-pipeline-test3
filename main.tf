6dcfe3f3d2be7e3267072d3e1bb9589b9bcf2b7a
gwenbleidd@32 <premiumforges32@gmail.com>
2024-05-04 11:11:06 -0700

    cicdtest3

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.27.0"
+    }
+  }
+}
+
+provider google {
+  # Configuration options
+  project= "pooper-scooper"
+  region= "europe-central2"
+  credentials= "poop.json"
+
+}
